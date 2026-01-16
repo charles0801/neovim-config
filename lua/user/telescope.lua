1a06c04d51a1804de95ce2f6f03587ab40dea339
charles0801 <wqypoiu@live.cn>
2026-01-16 13:41:59 +0800

    feat: enhance telescope find_files with hidden file support
    - Configure fd as file finder with custom options
    - Enable finding hidden files (--hidden flag)
    - Follow symlinks (--follow flag)
    - Exclude .git directory from search results
    - Improve file discovery experience

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -85,6 +85,16 @@ telescope.setup({
 		-- }
 		-- Now the picker_config_key will be applied every time you call this
 		-- builtin picker
+		find_files = {
+			"fd",
+			"--type",
+			"f",
+			"--color=never",
+			"--hidden",
+			"--follow",
+			"-E",
+			".git/*",
+		},
 		planets = {
 			show_pluto = true,
 		},
